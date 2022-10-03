58d9b6227c1b3444d5f99015a0cd2bf1d4e6c64d
KarenLyu21 <pkulyuwenhui@gmail.com>
2022-10-03 11:17:29 -0500

    seed.js

--- server/db/seeds/seed.js
+++ server/db/seeds/seed.js
@@ -1,21 +1,38 @@
 const mongoose = require('mongoose');
+const Term = require('../models/Term')
+console.log(Term)
+const List = require('../models/List')
+const Post = require('../models/Post')
+
 const dbUrl = process.env.ATLAS_URL || 'mongodb://localhost:27017/kant-map';
-mongoose.connect(dbUrl)
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+const connectMongoose = async () => {
+    await mongoose.connect(dbUrl);
+    const dataInDb = await Term.find({});
+    console.log('Current data under the schema', Term)
+    console.log(dataInDb)
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", () => {
+        console.log("Database connected");
+    });
+    db.on("close", () => {
+        console.log("Database closed");
+    })
+}
+connectMongoose();
 
 class SeedPair {
     constructor(schema, data) {
         this.schema = schema;
-        this.data = data
+        this.data = data;
+        // this.schema.deleteMany = schema.deleteMany;
+        // this.schema.find = schema.find;
     }
     async seed() {
         try {
             await this.schema.deleteMany({});
-            for (datum of this.data) {
+            for (let datum of this.data) {
+                console.log(datum)
                 let datumInDb = new this.schema(datum);
                 await datumInDb.save()
             }
@@ -25,20 +42,17 @@ class SeedPair {
             console.log(error)
         }    
     }
-    logResult() {
-        this.schema.find({}).then((dataInDb) => {
-            console.log(dataInDb)
-        })
+    async logResult() {
+        const dataInDb = await this.schema.find({})
+        console.log('Current data under the schema', this.schema)
+        console.log(dataInDb)
     }
 }
 
-const Term = require('../models/Term')
 const terms = require('./terms')
 const termPair = new SeedPair(Term, terms)
-const List = require('../models/List')
 const lists = require('./lists')
 const listPair = new SeedPair(List, lists)
-const Post = require('../models/Post')
 const posts = require('./posts')
 const postPair = new SeedPair(Post, posts)
 
@@ -46,7 +60,7 @@ const seedPairs = [termPair, listPair, postPair];
 
 const seedAll = async () => {
     for (let seedPair of seedPairs) {
-        seedPair.seed()
+        await seedPair.seed()
     }
 }
 
