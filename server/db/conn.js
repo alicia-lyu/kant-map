90981a58c57667d00f26f6f6d335d1bbb49f2469
KarenLyu21 <pkulyuwenhui@gmail.com>
2022-10-03 10:20:59 -0500

    db seeding

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dbUrl = process.env.ATLAS_URL || 'mongodb://localhost:27017/kant-map';
 const returnDb = () => {
+    mongoose.connect(dbUrl)
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error:"));
     db.once("open", () => {
--- server/db/seeds/seed.js
+++ server/db/seeds/seed.js
@@ -1,5 +1,11 @@
-const returnDb = require('../conn')
-const db = returnDb();
+const mongoose = require('mongoose');
+const dbUrl = process.env.ATLAS_URL || 'mongodb://localhost:27017/kant-map';
+mongoose.connect(dbUrl)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 class SeedPair {
     constructor(schema, data) {
@@ -13,22 +19,28 @@ class SeedPair {
                 let datumInDb = new this.schema(datum);
                 await datumInDb.save()
             }
+            console.log(`Seeding succeeded for ${this.schema}.`)
         } catch (error) {
-            console.log(`Seeding failed.`)
+            console.log(`Seeding failed for ${this.schema}.`)
             console.log(error)
         }    
+    }
+    logResult() {
+        this.schema.find({}).then((dataInDb) => {
+            console.log(dataInDb)
+        })
     }
 }
 
 const Term = require('../models/Term')
 const terms = require('./terms')
-const termPair = SeedPair(Term, terms)
+const termPair = new SeedPair(Term, terms)
 const List = require('../models/List')
 const lists = require('./lists')
-const listPair = SeedPair(List, lists)
+const listPair = new SeedPair(List, lists)
 const Post = require('../models/Post')
 const posts = require('./posts')
-const postPair = SeedPair(Post, posts)
+const postPair = new SeedPair(Post, posts)
 
 const seedPairs = [termPair, listPair, postPair];
 
@@ -38,12 +50,10 @@ const seedAll = async () => {
     }
 }
 
-seedAll.then(() => {
+seedAll().then(() => {
     mongoose.connection.close();
 });
 
 for (let seedPair of seedPairs) {
-    seedPair.schema.find({}).then((dataInDb) => {
-        console.log(dataInDb)
-    })
+    seedPair.logResult();
 }
