d575a4ec4f40599debfd00fa8fb0eecbc2ca8a8e
KarenLyu21 <pkulyuwenhui@gmail.com>
2022-10-03 14:52:20 -0500

    make seeding work

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,44 +1,15 @@
 const mongoose = require('mongoose');
 const dbUrl = process.env.ATLAS_URL || 'mongodb://localhost:27017/kant-map';
-const returnDb = () => {
-    mongoose.connect(dbUrl)
+const connectMongoose = async () => {
+    await mongoose.connect(dbUrl);
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error:"));
     db.once("open", () => {
         console.log("Database connected");
     });
-    return db
+    db.on("close", () => {
+        console.log("Database closed");
+    })
 }
 
-module.exports = returnDb
-
-
-
-
-// const { MongoClient } = require("mongodb");
-// const Db = process.env.ATLAS_URI;
-// const dbName = process.env.DB_NAME;
-// const client = new MongoClient(Db, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
- 
-// var _db;
- 
-// module.exports = {
-//   connectToServer: function (callback) {
-//     client.connect(function (err, db) {
-//       // Verify we got a good "db" object
-//       if (db)
-//       {
-//         _db = db.db(dbName);
-//         console.log("Successfully connected to MongoDB."); 
-//       }
-//       return callback(err);
-//          });
-//   },
- 
-//   getDb: function () {
-//     return _db;
-//   },
-// };
+module.exports = connectMongoose
--- server/db/seeds/seed.js
+++ server/db/seeds/seed.js
@@ -1,44 +1,21 @@
 const mongoose = require('mongoose');
-const Term = require('../models/Term')
-console.log(Term)
-const List = require('../models/List')
-const Post = require('../models/Post')
-
-const dbUrl = process.env.ATLAS_URL || 'mongodb://localhost:27017/kant-map';
-const connectMongoose = async () => {
-    await mongoose.connect(dbUrl);
-    const dataInDb = await Term.find({});
-    console.log('Current data under the schema', Term)
-    console.log(dataInDb)
-    const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error:"));
-    db.once("open", () => {
-        console.log("Database connected");
-    });
-    db.on("close", () => {
-        console.log("Database closed");
-    })
-}
-connectMongoose();
+const connectMongoose = require('../conn')
 
 class SeedPair {
     constructor(schema, data) {
         this.schema = schema;
         this.data = data;
-        // this.schema.deleteMany = schema.deleteMany;
-        // this.schema.find = schema.find;
     }
     async seed() {
         try {
             await this.schema.deleteMany({});
             for (let datum of this.data) {
-                console.log(datum)
                 let datumInDb = new this.schema(datum);
                 await datumInDb.save()
             }
-            console.log(`Seeding succeeded for ${this.schema}.`)
+            console.log('Seeding succeeded for', this.schema)
         } catch (error) {
-            console.log(`Seeding failed for ${this.schema}.`)
+            console.log('Seeding failed for', this.schema)
             console.log(error)
         }    
     }
@@ -49,10 +26,13 @@ class SeedPair {
     }
 }
 
+const Term = require('../models/Term')
 const terms = require('./terms')
 const termPair = new SeedPair(Term, terms)
+const List = require('../models/List')
 const lists = require('./lists')
 const listPair = new SeedPair(List, lists)
+const Post = require('../models/Post')
 const posts = require('./posts')
 const postPair = new SeedPair(Post, posts)
 
@@ -64,10 +44,17 @@ const seedAll = async () => {
     }
 }
 
-seedAll().then(() => {
-    mongoose.connection.close();
-});
-
-for (let seedPair of seedPairs) {
-    seedPair.logResult();
+const logAll = async () => {
+    for (let seedPair of seedPairs) {
+        await seedPair.logResult();
+    }
 }
+
+const seedNLog = async () => {
+    await connectMongoose();
+    await seedAll();
+    await logAll();
+    mongoose.connection.close();
+}
+
+seedNLog();
