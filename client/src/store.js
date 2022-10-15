a2cf54b5a728bb276d638c8172cb33fbf89714c6
KarenLyu21 <pkulyuwenhui@gmail.com>
2022-10-14 22:50:33 -0500

    unnecessary slices removed

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import termReducer from './slices/term.slice';
-import sentenceReducer from './slices/sentence.slice'
+// import termReducer from './slices/term.slice';
+// import sentenceReducer from './slices/sentence.slice'
 import messageReducer from './slices/message.slice'
 import authReducer from './slices/auth.slice'
 
