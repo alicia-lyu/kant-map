82a755af5615a8b9e9b36eadd2b043a6da4365d9
alicia-lyu <pkulyuwenhui@gmail.com>
2023-01-22 22:02:22 +0100

    display by authorization

--- server/routes/sentence.route.js
+++ server/routes/sentence.route.js
@@ -7,11 +7,11 @@ const termController = require("../controllers/term.controller")
 
 // routes
 
-sentenceRoutes.route('/:termName/sentences').get(controller.getSentences);
+sentenceRoutes.route('/:termName/sentences').get([authJwt.decodeToken], controller.getSentences);
 
 sentenceRoutes.route('/:termName/add-sentence').get([authJwt.verifyToken], termController.getTerm);
 
-sentenceRoutes.route('/:termName/:sentenceId').get(controller.findSentence);
+sentenceRoutes.route('/:termName/:sentenceId').get([authJwt.decodeToken], controller.findSentence);
 
 sentenceRoutes.route('/my-sentences').get([authJwt.verifyToken], controller.getMySentences);
 
